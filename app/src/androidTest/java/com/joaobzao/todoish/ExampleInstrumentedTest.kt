be287af61d1479257dea869760990185366151ae
joaobzao <jmmbzao@gmail.com>
2022-01-08 04:12:48 +0000

    create a jetpack compose  UITest to check if we are currently showing 'Hello Android' in the MainActivity Text

--- app/src/androidTest/java/com/joaobzao/todoish/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/joaobzao/todoish/ExampleInstrumentedTest.kt
@@ -1,5 +1,9 @@
 package com.joaobzao.todoish
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +25,13 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.joaobzao.todoish", appContext.packageName)
+    }
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun showHelloWorldInMainActivity() {
+        composeTestRule.onNodeWithText("Hello Android!").assertIsDisplayed()
     }
 }
