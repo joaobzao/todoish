a3bdad213f3984b108279d02854be60e9363dae8
joaobzao <jmmbzao@gmail.com>
2022-01-11 18:46:30 +0000

    create todo order sealed class

---
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/domain/util/TodoOrder.kt
@@ -0,0 +1,6 @@
+package com.joaobzao.todoish.feature_todo.domain.util
+
+sealed class TodoOrder(val orderType: OrderType) {
+    class Title(orderType: OrderType): TodoOrder(orderType)
+    class Date(orderType: OrderType): TodoOrder(orderType)
+}
