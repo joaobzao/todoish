266c87a7d02b0fd5115c1f83bf6f7e0ffaa0a61c
joaobzao <jmmbzao@gmail.com>
2022-01-11 03:29:09 +0000

    todo repository implementation

--- app/src/main/java/com/joaobzao/todoish/feature_todo/data/repository/TodoRepositoryImpl.kt
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/data/repository/TodoRepositoryImpl.kt
@@ -1,4 +1,24 @@
 package com.joaobzao.todoish.feature_todo.data.repository
 
-class TodoRepositoryImpl {
+import com.joaobzao.todoish.feature_todo.data.daos.TodoDao
+import com.joaobzao.todoish.feature_todo.data.entities.Todo
+import com.joaobzao.todoish.feature_todo.domain.repositories.TodoRepository
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepositoryImpl(
+    private val dao: TodoDao
+): TodoRepository {
+    override fun getTodos(): Flow<List<Todo>> =
+        dao.getTodos()
+
+    override suspend fun getTodoById(id: Long): Todo =
+        dao.getTodoById(id)
+
+    override suspend fun insertTodo(todo: Todo) {
+        dao.insertTodo(todo)
+    }
+
+    override suspend fun deleteTodo(todo: Todo) {
+        dao.deleteTodo(todo)
+    }
 }
