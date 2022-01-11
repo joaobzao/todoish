640d2788be19a62aad68f6f1d50ee7d50da99bf0
joaobzao <jmmbzao@gmail.com>
2022-01-11 22:41:03 +0000

    filter todos

--- app/src/main/java/com/joaobzao/todoish/feature_todo/domain/interactors/GetTodos.kt
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/domain/interactors/GetTodos.kt
@@ -2,14 +2,34 @@ package com.joaobzao.todoish.feature_todo.domain.interactors
 
 import com.joaobzao.todoish.feature_todo.data.entities.Todo
 import com.joaobzao.todoish.feature_todo.domain.repositories.TodoRepository
+import com.joaobzao.todoish.feature_todo.domain.util.OrderType
+import com.joaobzao.todoish.feature_todo.domain.util.TodoOrder
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetTodos(
     private val repository: TodoRepository
 ) {
 
-    operator fun invoke(): Flow<List<Todo>> {
+    operator fun invoke(
+        todoOrder: TodoOrder = TodoOrder.Date(OrderType.Descending)
+    ): Flow<List<Todo>> {
         return repository.getTodos()
+            .map { todos ->
+                when(todoOrder.orderType) {
+                    is OrderType.Ascending -> {
+                        when(todoOrder) {
+                            is TodoOrder.Title -> todos.sortedBy { it.title.lowercase() }
+                            is TodoOrder.Date -> todos.sortedBy { it.timestamp }
+                        }
+                    }
+                    is OrderType.Descending -> {
+                        when(todoOrder) {
+                            is TodoOrder.Title -> todos.sortedByDescending { it.title.lowercase() }
+                            is TodoOrder.Date -> todos.sortedByDescending { it.timestamp }
+                        }
+                    }
+                }
+            }
     }
-
 }
