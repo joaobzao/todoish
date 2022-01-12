b7eddcd5f5a7b47ab3f6f95df4b08cbbe71f7bff
joaobzao <jmmbzao@gmail.com>
2022-01-12 02:13:42 +0000

    create TodosViewState

---
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodosViewState.kt
@@ -0,0 +1,11 @@
+package com.joaobzao.todoish.feature_todo.presentation
+
+import com.joaobzao.todoish.feature_todo.data.entities.Todo
+import com.joaobzao.todoish.feature_todo.domain.util.OrderType
+import com.joaobzao.todoish.feature_todo.domain.util.TodoOrder
+
+data class TodosViewState(
+    val todos: List<Todo> = emptyList(),
+    val todoOrder: TodoOrder = TodoOrder.Date(OrderType.Descending)
+) {
+}
