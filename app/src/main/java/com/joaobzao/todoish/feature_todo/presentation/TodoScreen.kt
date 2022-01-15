17e05ab030c35108740b19003f5a0a74b10a361f
joaobzao <jmmbzao@gmail.com>
2022-01-15 03:05:23 +0000

    add fab to the screen, center it and dock it.

--- app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
@@ -1,7 +1,8 @@
 package com.joaobzao.todoish.feature_todo.presentation
 
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -11,14 +12,32 @@ fun TodoScreen(
     viewModel: TodosViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    val scaffoldState = rememberScaffoldState()
 
     TodoScaffold()
 }
 
 @Composable
 fun TodoScaffold() {
-    Text("Hello Todo Screen!")
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Todo"
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true,
+        // TODO: Check how this would work without a scaffoldState
+        scaffoldState = scaffoldState
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
