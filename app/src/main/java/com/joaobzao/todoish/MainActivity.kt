df6d45bba357296a64dcb5b789d3ff267e2b8264
joaobzao <jmmbzao@gmail.com>
2022-01-15 02:48:51 +0000

    Create the todo screen and plug it to the main activity

--- app/src/main/java/com/joaobzao/todoish/MainActivity.kt
+++ app/src/main/java/com/joaobzao/todoish/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.joaobzao.todoish.feature_todo.presentation.TodoScreen
 import com.joaobzao.todoish.ui.theme.TodoishTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +17,7 @@ class MainActivity : ComponentActivity() {
             TodoishTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    TodoScreen()
                 }
             }
         }
--- app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
@@ -1,4 +1,28 @@
 package com.joaobzao.todoish.feature_todo.presentation
 
-class TodoScreen {
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun TodoScreen(
+    viewModel: TodosViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState()
+
+    TodoScaffold()
+}
+
+@Composable
+fun TodoScaffold() {
+    Text("Hello Todo Screen!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoScreen_Preview() {
+    TodoScaffold()
 }
