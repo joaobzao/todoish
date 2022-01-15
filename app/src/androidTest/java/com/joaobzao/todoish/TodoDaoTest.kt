f12bcf8603cee11e67d85d3aefa1b761d8d200a8
joaobzao <jmmbzao@gmail.com>
2022-01-15 22:59:07 +0000

    create insert todo in db test

--- app/src/androidTest/java/com/joaobzao/todoish/TodoDaoTest.kt
+++ app/src/androidTest/java/com/joaobzao/todoish/TodoDaoTest.kt
@@ -1,4 +1,64 @@
 package com.joaobzao.todoish
 
-class TodoDaoTest {
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.joaobzao.todoish.feature_todo.data.TodoDatabase
+import com.joaobzao.todoish.feature_todo.data.daos.TodoDao
+import com.joaobzao.todoish.feature_todo.data.entities.Todo
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@ExperimentalCoroutinesApi
+class TodoDaoTest : DatabaseTest() {
+
+    private lateinit var todoDao: TodoDao
+    private lateinit var todoDatabase: TodoDatabase
+
+    @Before
+    fun initDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        todoDatabase = Room
+            .inMemoryDatabaseBuilder(context, TodoDatabase::class.java)
+            .setTransactionExecutor(testDispatcher.asExecutor())
+            .setQueryExecutor(testDispatcher.asExecutor())
+            .build()
+
+        todoDao = todoDatabase.todoDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        todoDatabase.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertTodoInDb() = testScope.runBlockingTest {
+        // Given
+        val todo = Todo(
+            id = 1L,
+            title = "test todo",
+            timestamp = 5L
+        )
+
+        // When
+        todoDao.insertTodo(todo)
+
+        // Then
+        assertThat(todoDao.getTodoById(todo.id), equalTo(todo))
+    }
+
+    @After
+    fun cleanup() {
+        testScope.cleanupTestCoroutines()
+    }
 }
