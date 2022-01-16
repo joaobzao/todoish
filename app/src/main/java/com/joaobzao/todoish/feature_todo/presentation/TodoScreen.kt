16ffb5eb1fa9d9e15bdfc0e6ae55b6a37ecb2d5c
joaobzao <jmmbzao@gmail.com>
2022-01-16 01:18:22 +0000

    BottomAppBar with burger button and docked fab

--- app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
+++ app/src/main/java/com/joaobzao/todoish/feature_todo/presentation/TodoScreen.kt
@@ -1,10 +1,21 @@
 package com.joaobzao.todoish.feature_todo.presentation
 
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -18,7 +29,7 @@ fun TodoScreen(
 
 @Composable
 fun TodoScaffold() {
-    val scaffoldState = rememberScaffoldState()
+    //val scaffoldState = rememberScaffoldState()
 
     Scaffold(
         floatingActionButton = {
@@ -33,10 +44,31 @@ fun TodoScaffold() {
         },
         floatingActionButtonPosition = FabPosition.Center,
         isFloatingActionButtonDocked = true,
-        // TODO: Check how this would work without a scaffoldState
-        scaffoldState = scaffoldState
+        bottomBar = { TodoBottomAppBar() }
     ) {
 
+    }
+}
+
+@Composable
+private fun TodoBottomAppBar() {
+    BottomAppBar(
+        Modifier.offset(Dp.Hairline),
+        cutoutShape = RoundedCornerShape(100),
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp
+            ),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent
+            )
+        ) {
+            Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+        }
     }
 }
 
